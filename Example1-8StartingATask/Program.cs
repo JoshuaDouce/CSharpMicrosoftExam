ebad9c1018b7a61c9822b245ed60416fe7d1786d
JoshuaDouce <Joshuadouce@gmail.com>
2018-04-07 14:31:49 +0100

    Example1-8 Starting A Task Added

---
+++ Example1-8StartingATask/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example1_8StartingATask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creates a new task and immediately starts it.
+            Task t = Task.Run(() => {
+                for (int i = 0; i < 100; i++)
+                {
+                    Console.WriteLine('*');
+                }
+            });
+
+            //waits until the task is finished before closing
+            t.Wait();
+        }
+    }
+}
