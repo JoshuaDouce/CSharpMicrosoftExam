a192a9dba52a09248206b55c0484bb8e941ff621
JoshuaDouce <Joshuadouce@gmail.com>
2018-04-07 10:44:42 +0100

    Add project files.

---
+++ CSharpMicrostExam/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace CSharpExam
+{
+    class Program
+    {
+        public static void ThreadMethod()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("ThreadProcess: {0}", i);
+                Thread.Sleep(2);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(new ThreadStart(ThreadMethod));
+            t.Start();
+
+            for (int i = 0; i < 4; i++)
+            {
+                System.Console.WriteLine("Main Thread: Do Some Work");
+                Thread.Sleep(3);
+            }
+
+            t.Join();
+        }
+    }
+}
---
+++ Example1-2BackgroundThread/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Example1_2BackgroundThread
+{
+    class Program
+    {
+        public static void ThreadMethod() {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("ThreadProcess: {0}" , i);
+                Thread.Sleep(1000);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(new ThreadStart(ThreadMethod));
+            //if true console app will exit immediately
+            //if false will execute as a foreground thread and will display count
+            t.IsBackground = false;
+            t.Start();
+        }
+    }
+}
