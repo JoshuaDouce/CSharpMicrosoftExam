3c3ba2fe9bc92f6b5644de1231078beec33c14d3
JoshuaDouce <Joshuadouce@gmail.com>
2018-04-07 17:59:59 +0100

    Example1-13Using A Task Factory

---
+++ Example1-13UsingATaskFactory/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example1_13UsingATaskFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task<Int32[]> parent = Task.Run(() => {
+                var results = new Int32[3];
+
+                TaskFactory tf = new TaskFactory(TaskCreationOptions.AttachedToParent, TaskContinuationOptions.ExecuteSynchronously);
+
+                tf.StartNew(() => results[0] = 0);
+                tf.StartNew(() => results[1] = 1);
+                tf.StartNew(() => results[2] = 2);
+
+                return results;
+            });
+
+            var finalTask = parent.ContinueWith(parentTask => {
+                foreach (var item in parentTask.Result)
+                {
+                    Console.WriteLine(item);
+                }
+            });
+
+            finalTask.Wait();
+        }
+    }
+}
