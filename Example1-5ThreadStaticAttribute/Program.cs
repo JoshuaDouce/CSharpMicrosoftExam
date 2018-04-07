9fb27b7ffd268549939f464291d298211dfa3fd8
JoshuaDouce <Joshuadouce@gmail.com>
2018-04-07 12:18:48 +0100

    Example1-5 ThreadStaticAttribute Added

---
+++ Example1-5ThreadStaticAttribute/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Example1_5ThreadStaticAttribute
+{
+    class Program
+    {
+        //Makes the static variable only available to the single thread. 
+        //Removing this allows each thread acces to same variable
+        [ThreadStatic]
+        public static int _field;
+
+        static void Main(string[] args)
+        {
+            new Thread(() => {
+                for (int i = 0; i < 10; i++)
+                {
+                    _field++;
+                    Console.WriteLine("Thread A: {0}", _field);
+                }
+            }).Start();
+
+            new Thread(() => {
+                for (int i = 0; i < 10; i++)
+                {
+                    _field++;
+                    Console.WriteLine("Thread B: {0}", _field);
+                }
+            }).Start();
+
+            Console.ReadKey();
+        }
+    }
+}
