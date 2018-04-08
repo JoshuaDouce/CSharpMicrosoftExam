9e219dfd0eb40557932a704979d51e3b3d4e45b6
JoshuaDouce <Joshuadouce@gmail.com>
2018-04-08 09:31:20 +0100

    Example1-18 Added Asncy and Await

---
+++ Example1-18AsyncAwait/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Example1_18AsyncAwait
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //accesss result on main thread which blocks teh code until the async method completes
+            string result = DownloadContent().Result;
+            Console.WriteLine(result);
+        }
+
+        public static async Task<string> DownloadContent() {
+            using (HttpClient client = new HttpClient()) {
+                //GetStringAsync uses asynchronous code internally and returns Task<string>
+                string result = await client.GetStringAsync("http://www.microsoft.com");
+                return result;
+            }
+        }
+    }
+}
