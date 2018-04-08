c82003cd3cc2ffb07646260c067fe8fff5f782a1
JoshuaDouce <Joshuadouce@gmail.com>
2018-04-08 09:10:48 +0100

    Example1-16 Using parallel for and for each

---
+++ Example1-16UsingParallelForAndForeach/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+
+namespace Example1_16UsingParallelForAndForeach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parallel.For(0, 10, i=> {
+                Console.WriteLine(i);
+                Thread.Sleep(1000);
+            });
+
+            var numbers = Enumerable.Range(0, 10);
+            Parallel.ForEach(numbers, i => {
+                Console.WriteLine(i);
+                Thread.Sleep(100);
+            });
+        }
+    }
+}
