7f003951a97bafaf392d74952d7e868c229bc5dd
JoshuaDouce <Joshuadouce@gmail.com>
2018-04-08 09:42:36 +0100

    Example1-19 added Scalability vs Responsiveness

---
+++ Example1-19ScalabilityVSResponsiveness/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Example1_19ScalabilityVSResponsiveness
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        //Uses thread from a threadpool while sleeping
+        public Task SleepAsyncA(int millisecondTimeout) {
+            return Task.Run(() => Thread.Sleep(millisecondTimeout));
+        }
+
+        //Does not occupy a thread while waiting for the timer to run. Giving you scalability
+        public Task SleepAsyncB(int millisecondTimeout) {
+            TaskCompletionSource<bool> tcs = null;
+            var t = new Timer(delegate { tcs.TrySetResult(true); }, null, -1, -1);
+            tcs = new TaskCompletionSource<bool>(t);
+            t.Change(millisecondTimeout, -1);
+            return tcs.Task;
+        }
+    }
+}
