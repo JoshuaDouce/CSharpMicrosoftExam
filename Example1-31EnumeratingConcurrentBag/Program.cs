aa7aee2fec595ae6bca9eca5d5207c49a9d143c6
JoshuaDouce <Joshuadouce@gmail.com>
2018-04-09 20:02:50 +0100

    Example 1-278 to 1-31 added

---
+++ Example1-31EnumeratingConcurrentBag/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Collections.Concurrent;
+
+namespace Example1_31EnumeratingConcurrentBag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConcurrentBag<int> bag = new ConcurrentBag<int>();
+
+            Task.Run(() => {
+                bag.Add(42);
+                Thread.Sleep(1000);
+                bag.Add(21);
+            });
+
+            Task.Run(() => {
+                foreach (var i in bag)
+                {
+                    Console.WriteLine(i);
+                }
+            }).Wait();
+        }
+    }
+}
