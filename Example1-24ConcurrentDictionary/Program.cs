726bec3052a6f95d3fc7670df690810d4d9d190a
JoshuaDouce <Joshuadouce@gmail.com>
2018-04-10 18:25:40 +0100

    Chpater1 - Objective 1 examples completed

---
+++ Example1-24ConcurrentDictionary/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example1_24ConcurrentDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dict = new ConcurrentDictionary<string, int>();
+
+            if (dict.TryAdd("k1", 42))
+            {
+                Console.WriteLine("Added");
+            }
+
+            if (dict.TryUpdate("k1", 21, 42))
+            {
+                Console.WriteLine("42 updated to 21");
+            }
+
+            dict["k1"] = 42; //overrite unconditionally
+
+            int r1 = dict.AddOrUpdate("k1", 3, (s, i) => i * 2);
+            int r2 = dict.GetOrAdd("k2", 3);
+        }
+    }
+}
---
+++ Example1-33ConcurrentQueue/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example1_33ConcurrentQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConcurrentQueue<int> queue = new ConcurrentQueue<int>();
+            queue.Enqueue(42);
+
+            int result;
+
+            if (queue.TryDequeue(out result))
+            {
+                Console.WriteLine("Dequeued {0}", result);
+            }
+        }
+    }
+}
