fd31a329c4479e882462bfb01a500dc2246b40dd
Joshua Douce <joshuadouce@gmail.com>
2018-05-01 09:09:59 +0100

    Example 2-58 and 2-74 added

---
+++ Example2-74UsingTheCodeDOM/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.CodeDom;
+using Microsoft.CSharp;
+using System.IO;
+using System.CodeDom.Compiler;
+
+namespace Example2_74UsingTheCodeDOM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CodeCompileUnit CCUnit = new CodeCompileUnit();
+            CodeNamespace myNamespace = new CodeNamespace("MyNamespace");
+            myNamespace.Imports.Add(new CodeNamespaceImport("System"));
+            CodeTypeDeclaration myClass = new CodeTypeDeclaration("MyClass");
+            CodeEntryPointMethod start = new CodeEntryPointMethod();
+            CodeMethodInvokeExpression cs1 = new CodeMethodInvokeExpression(
+                new CodeTypeReferenceExpression("Console"), "WriteLine", new CodePrimitiveExpression("Hello World!"));
+
+            CCUnit.Namespaces.Add(myNamespace);
+            myNamespace.Types.Add(myClass);
+            myClass.Members.Add(start);
+            start.Statements.Add(cs1);
+
+            CSharpCodeProvider provider = new CSharpCodeProvider();
+
+            using (StreamWriter sw = new StreamWriter("Helloworld.cs", false))
+            {
+                IndentedTextWriter tw = new IndentedTextWriter(sw, "     ");
+                provider.GenerateCodeFromCompileUnit(CCUnit, tw, new CodeGeneratorOptions());
+            }
+        }
+    }
+}
