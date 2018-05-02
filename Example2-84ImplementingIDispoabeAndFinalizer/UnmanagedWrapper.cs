1f67d94c1a950e9b961f36499f5e49e9a3fa25c6
Joshua Douce <joshuadouce@gmail.com>
2018-05-02 09:03:13 +0100

    Objective2.6 Completed

---
+++ Example2-84ImplementingIDispoabeAndFinalizer/UnmanagedWrapper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace Example2_84ImplementingIDispoabeAndFinalizer
+{
+    class UnmanagedWrapper : IDisposable
+    {
+        private IntPtr unmanagedBuffer;
+        public FileStream Stream { get; private set; }
+
+        public UnmanagedWrapper() {
+            CreateBuffer();
+            this.Stream = File.Open("temp.dat", FileMode.Create);
+        }
+
+        private void CreateBuffer()
+        {
+            byte[] data = new byte[1024];
+            new Random().NextBytes(data);
+            unmanagedBuffer = Marshal.AllocHGlobal(data.Length);
+            Marshal.Copy(data, 0, unmanagedBuffer, data.Length);
+        }
+
+        ~UnmanagedWrapper() {
+            Dispose(false);
+        }
+
+        public void Close() {
+            Dispose();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing) {
+            Marshal.FreeHGlobal(unmanagedBuffer);
+            if (disposing)
+            {
+                if (Stream != null)
+                {
+                    Stream.Close();
+                }
+            }
+        }
+    }
+}
