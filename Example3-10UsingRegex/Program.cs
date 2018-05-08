75c3c7544039e9106f0fb37aa5d144c6e4df567e
Joshua Douce <joshuadouce@gmail.com>
2018-05-08 20:54:28 +0100

    Example 3-10 and 3-11 added

---
+++ Example3-10UsingRegex/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Example3_10UsingRegex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static bool ValidatZipCode(string zipcode) {
+            Match match = Regex.Match(zipcode, @"^[1-9][0-9]{3}\s?[a-zA-Z]{2}$", RegexOptions.IgnoreCase);
+            return match.Success;
+        }
+    }
+}
---
+++ Example3-11CollapsingMultipleWhiteSpace/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Example3_11CollapsingMultipleWhiteSpace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RegexOptions opts = RegexOptions.None;
+            Regex regex = new Regex(@"[ ]{2,}", opts);
+
+            string input = "1 2 3    4 5";
+            string result = regex.Replace(input, " ");
+
+            Console.WriteLine(result);
+        }
+    }
+}
