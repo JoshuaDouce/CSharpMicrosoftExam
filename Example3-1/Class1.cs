7d7acdabd4b9f40266774df1475a0ffc9abf9e2f
JoshuaDouce <Joshuadouce@gmail.com>
2018-05-08 15:46:28 +0100

    Example3-1 Added

---
+++ Example3-1/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Example3_1
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        [Required, MaxLength(20)]
+        public string FirstName { get; set; }
+    }
+}
