12f8063f5fb25ac6cdc27d51798d01c7d2f73346
Joshua Douce <joshuadouce@gmail.com>
2018-05-14 20:55:53 +0100

    Chapter 4 objective 1 Example 4-1 and 4-2 added

---
+++ Example4-1ListingDriveInfo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Example4_1ListingDriveInfo
+{
+    class Program
+    {
+        private static DriveInfo[] drivesInfo = DriveInfo.GetDrives();
+
+        static void Main(string[] args)
+        {
+            foreach (DriveInfo driveInfo in drivesInfo)
+            {
+                Console.WriteLine("Drive {0}", driveInfo.Name);
+                Console.WriteLine("  Filter type: {0}", driveInfo.DriveType);
+
+                if (driveInfo.IsReady == true)
+                {
+                    Console.WriteLine("  Volume label: {0}", driveInfo.VolumeLabel);
+                    Console.WriteLine("  FileSystem: {0}", driveInfo.DriveFormat);
+                    Console.WriteLine("  Available space to user: {0, 15} bytes", driveInfo.AvailableFreeSpace);
+                    Console.WriteLine("  Total Available space: {0, 15} bytes", driveInfo.TotalFreeSpace);
+                    Console.WriteLine("  Total drive size: {0, 15} bytes", driveInfo.TotalSize);
+                }
+            }
+        }
+    }
+}
---
+++ Example4-2CreatingADirectory/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example4_2CreatingADirectory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var directory = Directory.CreateDirectory(@"C:\Temp\ProgrammingInCSharp\NewDirectory");
+
+            var directoryInfo = new DirectoryInfo(@"C:\Temp\ProgrammingInCSharp\DirectoryInfo");
+            directoryInfo.Create();
+        }
+    }
+}
