80f34829487c01fe255634d493be2aef23f52546
JoshuaDouce <Joshuadouce@gmail.com>
2018-05-17 10:28:19 +0100

    Example1-6 Additional comments and example added

--- Example1-6ThreadLocalT/Program.cs
+++ Example1-6ThreadLocalT/Program.cs
@@ -6,24 +6,37 @@ using System.Threading;
 
 namespace Example1_6ThreadLocalT
 {
-    class Program
+    internal class Program
     {
-        public static ThreadLocal<int> _field = new ThreadLocal<int>(() => {
-            return Thread.CurrentThread.ManagedThreadId;
-        });
+        //uses local data in a thread and initializes it for each thread
+        //Create new threadLocal variable with a type of int and initialize it
+        //This variable contains the current threads managed thread ID.
+        public static ThreadLocal<int> _field = new ThreadLocal<int>(() => Thread.CurrentThread.ManagedThreadId);
+        public static ThreadLocal<string> _text = new ThreadLocal<string>(() => "Local Variable");
 
-        static void Main(string[] args){
+        private static void Main(string[] args){
             new Thread(() => {
-                for (int i = 0; i < _field.Value; i++)
+                for (var i = 0; i < _field.Value; i++)
                 {
-                    Console.WriteLine("Thread A: {0}", i);
+                    Console.WriteLine($"Thread A: {i}");
+                    Console.WriteLine($"Thread A's {_text}");
                 }
             }).Start();
 
             new Thread(() => {
-                for (int i = 0; i < _field.Value; i++)
+                for (var i = 0; i < _field.Value; i++)
                 {
-                    Console.WriteLine("Thread B: {0}", i);
+                    Console.WriteLine($"Thread B: {i}");
+                    Console.WriteLine($"Thread B's {_text}");
+                }
+            }).Start();
+
+            new Thread(() =>
+            {
+                for (var i = 0; i < _field.Value; i++)
+                {
+                    Console.WriteLine($"Thread C: {i}");
+                    Console.WriteLine($"Thread C's {_text}");
                 }
             }).Start();
 
