c8f471acc93efb0055586cd97f944fb49c7377b7
JoshuaDouce <Joshuadouce@gmail.com>
2018-05-17 11:14:14 +0100

    Additonal Comments added to example 1-7

--- Example1-7QueuingUsingThreadPool/Program.cs
+++ Example1-7QueuingUsingThreadPool/Program.cs
@@ -6,12 +6,21 @@ using System.Threading;
 
 namespace Example1_7QueuingUsingThreadPool
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            //The thread pool manages your threads for you, This shows the thread pool queing a new item
+            //This is useful in a web service for example where you dont want to manually spin up a thread for each
+            //request. This could easily crash the server.
+            //The thread pool will manage the number of threads and queue any tasks until a thread becomes available.
             ThreadPool.QueueUserWorkItem((s) => {
                 Console.WriteLine("Working on a thread from threadpool");
+            });
+
+            ThreadPool.QueueUserWorkItem((t) =>
+            {
+                Console.WriteLine("Working on another thread from the thread pool");
             });
 
             Console.ReadLine();
