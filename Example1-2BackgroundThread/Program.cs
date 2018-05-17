8aa111d7044dbd47ae397f179094d422b2306d22
JoshuaDouce <Joshuadouce@gmail.com>
2018-05-17 09:54:20 +0100

    Additional comments added to example 1-2

--- Example1-2BackgroundThread/Program.cs
+++ Example1-2BackgroundThread/Program.cs
@@ -6,22 +6,25 @@ using System.Threading;
 
 namespace Example1_2BackgroundThread
 {
-    class Program
+    internal class Program
     {
         public static void ThreadMethod() {
-            for (int i = 0; i < 10; i++)
+            for (var i = 0; i < 10; i++)
             {
-                Console.WriteLine("ThreadProcess: {0}" , i);
+                Console.WriteLine($"ThreadProcess: {i}");
                 Thread.Sleep(1000);
             }
         }
 
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Thread t = new Thread(new ThreadStart(ThreadMethod));
-            //if true console app will exit immediately
-            //if false will execute as a foreground thread and will display count
-            t.IsBackground = false;
+            var t = new Thread(ThreadMethod)
+            {
+                //This setting specifies if the tread is a background or a foreground thread.
+                //If true console app will exit immediately as when all foreground threads finish the app terminates
+                //if false will execute as a foreground thread and will display the count
+                IsBackground = false
+            };
             t.Start();
         }
     }
