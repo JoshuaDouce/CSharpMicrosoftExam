8f5949562be4412ee70768bcd27f0398ac43bd4a
JoshuaDouce <Joshuadouce@gmail.com>
2018-05-23 22:03:48 +0100

    More revision notes added

---
+++ Question25-1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question25_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public class Product {
+
+        }
+
+        //Specifies a strongly typed save method which allows only types inherited from the product class that
+        //use a constructor with no params
+        //Save<T> amkes it strongly type
+        //Where T:Product specifies it must be a product or inherit from one
+        //new() specifies it must have an empty constructor
+        public static void Save<T>(T target) where T : Product, new()
+        {
+
+        }
+    }
+}
---
+++ UsingJavascriptSerializer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.UI;
+using System.Web.Script.Serialization;
+
+namespace UsingJavascriptSerializer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public class Name
+        {
+            public int[] Values { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+        }
+
+        public static Name ConvertToName(string json) {
+
+            var ser = new JavaScriptSerializer();
+
+            //<class deserialize to>(string to deserialize)
+            return ser.Deserialize<Name>(json);
+        }
+    }
+}
